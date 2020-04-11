ab051a5bff1c3ffb9d6622290f388af868261e82
thisisjuke <liege.arthur60@gmail.com>
2020-04-11 19:11:51 +0200

    :tada: init project | front nuxt - back strapi

---
+++ front/pages/shops/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <input v-model="query" type="search" placeholder="Search...">
+
+    <div v-for="shop in filteredList" v-bind:key="shop.id">
+      {{ shop }}
+    </div>
+
+    <div v-if="filteredList.length === 0">
+      <p>No results</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  // Import the restaurants query
+  import shopsQuery from '../../apollo/queries/shop/shops.gql'
+
+  export default {
+    data() {
+      return {
+        shops: [],
+        query: ''
+      }
+    },
+    apollo: {
+      shops: {
+        prefetch: true,
+        query: shopsQuery
+      }
+    },
+    computed: {
+      filteredList() {
+        return this.shops.filter(shop => {
+          return shop.name.toLowerCase().includes(this.query.toLowerCase())
+        })
+      },
+    }
+  }
+</script>
