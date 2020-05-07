f38643da0b0e39f262437f534547faa7fdbb9877
thisisjuke <liege.arthur60@gmail.com>
2020-05-08 00:28:45 +0200

    :sparkles: add search routes and default route with random stores and products

--- back/api/search/controllers/search.js
+++ back/api/search/controllers/search.js
@@ -1,19 +1,33 @@
+const { sanitizeEntity } = require('strapi-utils');
+
+const SHOP_COLUMNS = ["description", "name"]
+const PRODUCT_COLUMNS = ["description", "name", "target"]
+
 module.exports = {
-  async find(ctx) {
-    const shops = await strapi.services.search.entityWithParams(ctx.query, 'shop');
-    const products = await strapi.services.search.entityWithParams(ctx.query, 'product');
+  async findAll() {
+    const shops = await strapi.services.search.randomQueryOutput('shop', 10);
+    const products = await strapi.services.search.randomQueryOutput('product', 10);
 
     return { shops, products };
   },
-  async findShops(ctx) {
-    const shops = await strapi.services.search.entityWithParams(ctx.query, 'shop');
+  async searchAll(ctx) {
+    const { query } = ctx.params;
+    const shops = await strapi.services.search.entityWithQuery(query, 'shop', SHOP_COLUMNS);
+    const products = await strapi.services.search.entityWithQuery(query, 'product', PRODUCT_COLUMNS);
+
+    return { shops, products };
+  },
+  async searchShops(ctx) {
+    const { query } = ctx.params;
+    const shops = await strapi.services.search.entityWithQuery(query, 'shop', SHOP_COLUMNS);
 
     return { shops };
   },
-  async findProducts(ctx) {
-    const products = await strapi.services.search.entityWithParams(ctx.query, 'product');
+  async searchProducts(ctx) {
+    const { query } = ctx.params;
+    const products = await strapi.services.search.entityWithQuery(query, 'product', PRODUCT_COLUMNS);
 
     return { products };
-  }
+  },
 };
 
--- back/api/search/services/search.js
+++ back/api/search/services/search.js
@@ -1,15 +1,21 @@
 'use strict';
-const { sanitizeEntity } = require('strapi-utils');
 
-const entityWithParams = async (params, entity) => {
-  const res = await strapi.query(entity).find(params);
-  const sanitizedRes = sanitizeEntity(res.toJSON ? res.toJSON() : res, {
-    model: strapi.query(entity).model,
-  });
-
-  return Object.keys(sanitizedRes).map(i => sanitizedRes[i]);
+const entityWithQuery = async (query, entity, columns) => {
+  return strapi.query(entity).model.find( { $or: getParamsFromQuery(query, columns) } ).limit(15)
 };
 
+const randomQueryOutput = async (entity, size) => {
+  return strapi.query(entity).model.aggregate([ { $sample: { size } } ])
+}
+
+const getParamsFromQuery = (query, columns) => {
+  const res = []
+  columns.forEach(column => res.push({[column]: { "$regex": query, "$options": "i" } }))
+
+  return res
+}
+
 module.exports = {
-  entityWithParams
+  entityWithQuery,
+  randomQueryOutput
 };
