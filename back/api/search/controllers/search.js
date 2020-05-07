882bc5dbe6e76ddf7619d2e1976f553b75dcf573
thisisjuke <liege.arthur60@gmail.com>
2020-05-07 18:28:08 +0200

    :recycle: split search api in services

--- back/api/search/controllers/search.js
+++ back/api/search/controllers/search.js
@@ -1,22 +1,13 @@
-const { sanitizeEntity } = require('strapi-utils');
-
 module.exports = {
   async find(ctx) {
     const params = ctx.query;
 
-    const shops = await strapi.query('shop').find(params);
-    const sanitizedShops = sanitizeEntity(shops.toJSON ? shops.toJSON() : shops, {
-      model: strapi.query('shop').model,
-    });
-
-    const products = await strapi.query('product').find(params);
-    const sanitizedProducts = sanitizeEntity(products.toJSON ? shops.toJSON() : products, {
-      model: strapi.query('product').model,
-    });
+    const shops = await strapi.services.search.entityWithParams(params, 'shop');
+    const products = await strapi.services.search.entityWithParams(params, 'product');
 
     return {
-      shops: Object.keys(sanitizedShops).map(i => sanitizedShops[i]),
-      products: Object.keys(sanitizedProducts).map(i => sanitizedProducts[i])
+      shops,
+      products,
     }
   }
 };
--- back/api/search/services/search.js
+++ back/api/search/services/search.js
@@ -1,3 +1,15 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
-module.exports = {};
+const entityWithParams = async (params, entity) => {
+  const res = await strapi.query(entity).find(params);
+  const sanitizedRes = sanitizeEntity(res.toJSON ? res.toJSON() : res, {
+    model: strapi.query(entity).model,
+  });
+
+  return Object.keys(sanitizedRes).map(i => sanitizedRes[i])
+};
+
+module.exports = {
+  entityWithParams
+};
