2fd361d00588040a6126e9c78cc5588f88efa376
thisisjuke <liege.arthur60@gmail.com>
2020-05-07 22:48:38 +0200

    :sparkles: search api v1 : only products/shops

--- back/api/search/controllers/search.js
+++ back/api/search/controllers/search.js
@@ -1,14 +1,19 @@
 module.exports = {
   async find(ctx) {
-    const params = ctx.query;
+    const shops = await strapi.services.search.entityWithParams(ctx.query, 'shop');
+    const products = await strapi.services.search.entityWithParams(ctx.query, 'product');
 
-    const shops = await strapi.services.search.entityWithParams(params, 'shop');
-    const products = await strapi.services.search.entityWithParams(params, 'product');
+    return { shops, products };
+  },
+  async findShops(ctx) {
+    const shops = await strapi.services.search.entityWithParams(ctx.query, 'shop');
 
-    return {
-      shops,
-      products,
-    };
+    return { shops };
+  },
+  async findProducts(ctx) {
+    const products = await strapi.services.search.entityWithParams(ctx.query, 'product');
+
+    return { products };
   }
 };
 
