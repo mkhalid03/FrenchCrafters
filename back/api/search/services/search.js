70f8d3e3b59f268c6c4fb37758dc768223c921ca
thisisjuke <liege.arthur60@gmail.com>
2020-05-08 19:47:11 +0200

    :recycle: refactor search api to filter targets by keyword

--- back/api/search/services/search.js
+++ back/api/search/services/search.js
@@ -1,5 +1,5 @@
 'use strict';
-const i18n = require('i18n'), path = require("path")
+const i18n = require('i18n'), path = require("path"), translation = require('../../../config/locales/fr_fr.json')
 
 i18n.configure({
   locales:['fr_fr'],
@@ -9,7 +9,7 @@ i18n.configure({
 });
 
 const entityWithQuery = async (query, entity, columns) => {
-  return strapi.query(entity).model.find( { $or: getParamsFromQuery(query, columns) } ).limit(15)
+  return strapi.query(entity).model.find( getParamsFromQuery(query, columns) )
 };
 
 const randomQueryOutput = async (entity, size) => {
@@ -19,22 +19,28 @@ const randomQueryOutput = async (entity, size) => {
 const getParamsFromQuery = (query, columns) => {
 
   const queryArray = query.split(' ');
-  const res = []
+  const contentFilter = []
+  const targetFilter = []
+
   columns.forEach(column => {
-    if(column === "target"){
+    if(column !== "target"){
       queryArray.forEach(q => {
-        res.push({[column]: { "$regex": __({phrase: q, locale: 'fr_fr'}), "$options": "i" } })
+        contentFilter.push({[column]: { "$regex": q, "$options": "i" } })
       })
     } else {
       queryArray.forEach(q => {
-        res.push({[column]: { "$regex": q, "$options": "i" } })
+        if(q in translation){
+          targetFilter.push({target: { "$regex": __({phrase: q, locale: 'fr_fr'}), "$options": "i" } })
+        }
       })
     }
   })
 
-  console.log(res)
+  if(targetFilter.length === 0) {
+    return { $and: [{ $or: contentFilter }]}
+  }
 
-  return res
+  return { $and: [{ $or: contentFilter }, { $or: targetFilter }]}
 }
 
 module.exports = {
