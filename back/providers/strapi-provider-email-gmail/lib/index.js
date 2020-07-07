3186f8a4d50a5854272ad57e1fd4fe11cdef7632
thisisjuke <liege.arthur60@gmail.com>
2020-07-07 14:23:44 +0200

    :bug: smtp fix

--- back/providers/strapi-provider-email-gmail/lib/index.js
+++ back/providers/strapi-provider-email-gmail/lib/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const nodemailer = require('nodemailer');
-const { removeUndefined } = require('strapi-utils');
+const {removeUndefined} = require('strapi-utils');
 
 module.exports = {
   init: (providerOptions = {}, settings = {}) => {
@@ -9,30 +9,22 @@ module.exports = {
       ...providerOptions
     });
     return {
-      send: options => {
-        return new Promise((resolve, reject) => {
-          const { from, to, cc, bcc, replyTo, subject, text, html, ...rest } = options;
+      send: async options => {
+        const {from, to, cc, bcc, replyTo, subject, text, html, ...rest} = options;
 
-          let msg = {
-            from: from || settings.defaultFrom,
-            to,
-            cc,
-            bcc,
-            replyTo: replyTo || settings.defaultReplyTo,
-            subject,
-            text,
-            html,
-            ...rest,
-          };
+        let msg = {
+          from: from || settings.defaultFrom,
+          to,
+          cc,
+          bcc,
+          replyTo: replyTo || settings.defaultReplyTo,
+          subject,
+          text,
+          html,
+          ...rest,
+        };
 
-          transporter.sendMail(removeUndefined(msg), function(error, info){
-            if (error) {
-              console.log(error);
-            } else {
-              console.log('Email sent: ' + info.response);
-            }
-          });
-        });
+        return await transporter.sendMail(removeUndefined(msg))
       },
     };
   },
