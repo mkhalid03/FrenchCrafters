e1caf791d812da070b9d86e5612542553f0b0a31
thisisjuke <liege.arthur60@gmail.com>
2020-07-15 01:11:19 +0200

    :sparkles: add message when no search results

--- front/components/products/ProductsDisplay.vue
+++ front/components/products/ProductsDisplay.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="products">
+    <div v-if="products" class="mt-8">
       <h2 class="text-2xl ml-4">Les produits :</h2>
       <div class="ml-4 text-gray-800 my-6">
         FrenchCrafters propose à ses partenaires de vendre sur la plateforme leurs produits phares !
@@ -10,6 +10,10 @@
       <el-divider content-position="left">Résultats de votre Recherche :</el-divider>
       <div class="w-full flex flex-wrap">
         <ProductCard v-for="product in products" :key="product.id" :product="product" />
+      </div>
+      <div v-if="products.length === 0" class="text-center mt-4">
+        <div class="text-xl text-gray-800 text-bold">Oops.. Il semblerait qu'aucun produit ne corresponde à votre demande.</div>
+        <div class="text-md text-gray-600">Vous devriez essayer d'effectuer une recherche avec moins de filtres.</div>
       </div>
     </div>
 </template>
--- front/components/shops/ShopsDisplay.vue
+++ front/components/shops/ShopsDisplay.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="shops" class="my-8">
+  <div v-if="shops" class="mt-8">
     <h2 class="text-2xl ml-4">Les Magasins :</h2>
     <div class="ml-4 text-gray-800 my-6">
       FrenchCrafters référence des magasins Français faisant du Made in France, et souhaitant vous partager leur univers, leur façon d'inover ainsi que leurs produits !
@@ -10,6 +10,10 @@
     <el-divider content-position="left">Nos Magasins partenaires :</el-divider>
     <div class="pl-4 pr-8 w-full flex flex-wrap">
       <ShopCard v-for="shop in shops" :key="shop.id" :shop="shop" />
+    </div>
+    <div v-if="shops.length === 0" class="text-center mt-4">
+      <div class="text-xl text-gray-800 text-bold">Oops.. Il semblerait qu'aucun magasin ne corresponde à votre demande.</div>
+      <div class="text-md text-gray-600">Les mots recherchés ne sont peut-être liés à aucun magasin. Tentez une recherche avec des termes plus génériques ?</div>
     </div>
   </div>
 </template>
