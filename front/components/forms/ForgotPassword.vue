0379003d34ef1a43226ece542b5e27f30b105f9f
thisisjuke <liege.arthur60@gmail.com>
2020-07-15 19:51:59 +0200

    :lipstick: resetpassword style

--- front/components/forms/ForgotPassword.vue
+++ front/components/forms/ForgotPassword.vue
@@ -1,46 +1,37 @@
 <template>
   <div>
-    <div v-if="!isSent">
-      <form @submit.stop.prevent="sendResetMail">
-        <label for="resetMail">Your account email :</label>
-        <input id="resetMail" v-model="resetMail" type="email" >
-        <button type="submit">
-          Reset my password
-        </button>
-      </form>
-    </div>
-    <div v-else>
-      Mail Sent !
-    </div>
+    <label for="resetMail">Email de votre compte :</label>
+    <ElInput class="mb-4" id="resetMail" v-model="resetMail" type="email"/>
+    <ElButton type="primary" @click="sendResetMail">
+      Réinitialiser mon Mot de passe !
+    </ElButton>
   </div>
 </template>
 
 <script>
-import axios from "axios"
+  import axios from "axios"
 
-export default {
-  data: function () {
-    return {
-      isSent: false,
-      resetMail: "",
-    }
-  },
-  methods: {
-    sendResetMail() {
-      axios
-        .post(`${process.env.backendUrl}/auth/forgot-password`, {
-          email: this.resetMail,
-          url: "/reset-password",
-        })
-        .then((response) => {
-          // Handle success.
-          console.log("Your user received an email")
-        })
-        .catch((error) => {
-          // Handle error.
-          console.log("An error occurred:", error)
-        })
+  export default {
+    data: function () {
+      return {
+        resetMail: "",
+      }
     },
-  },
-}
+    methods: {
+      sendResetMail() {
+        if(this.resetMail === "")
+        axios
+          .post(`${process.env.backendUrl}/auth/forgot-password`, {
+            email: this.resetMail,
+            url: "/reset-password",
+          })
+          .then((response) => {
+            this.$message('Un mail a été envoyé à cette adresse');
+          })
+          .catch((error) => {
+            this.$message('Une erreur est survenue durant la réinialisation. Votre email est-il valide ?');
+          })
+      },
+    },
+  }
 </script>
