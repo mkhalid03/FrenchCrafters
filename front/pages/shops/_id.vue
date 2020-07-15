0fb03bc9123e515c4ee8837e4969d24a8d95e123
thisisjuke <liege.arthur60@gmail.com>
2020-07-16 00:21:12 +0200

    :lipstick: simple shop page

--- front/pages/shops/_id.vue
+++ front/pages/shops/_id.vue
@@ -1,48 +1,58 @@
 <template>
-  <div>
-    <ElButton @click="$router.go(-1)">
-      go back
-    </ElButton>
+  <div class="px-4">
+    <div class="m-4 pt-8" >
+      <ElButton @click="$router.go(-1)">
+        go back
+      </ElButton>
+    </div>
+
+    <h2 class="text-4xl ml-4 font-mono">{{shop.name}}</h2>
+    <div class="w-2/3 ml-4 text-gray-800 mt-6 mb-12">
+      {{shop.description}}
+    </div>
+
+    <el-divider content-position="left">Les articles disponibles chez {{shop.name}} :</el-divider>
 
     <client-only>
-      <div v-for="product in shop.products">
-        <ProductCard :product="product" />
+      <div class="px-24">
+        <div class="w-full flex flex-wrap">
+          <ProductCard :product="product" v-for="product in shop.products" :key="product.id"/>
+        </div>
       </div>
-      <Cart />
     </client-only>
   </div>
 </template>
 
 <script>
-import { mapMutations } from "vuex"
-import Cart from "~/components/Cart.vue"
-import ProductCard from "../../components/products/ProductCard";
+  import {mapMutations} from "vuex"
+  import Cart from "~/components/Cart.vue"
+  import ProductCard from "../../components/products/ProductCard";
 
-export default {
-  head () {
-    return {
-      title: `${this.shop.name} | FrenchCrafters`,
-      meta: [
-        { hid: 'checkout', name: 'description', content: `FrenchCrafters shop page for ${this.shop.name}` }
-      ]
-    }
-  },
-  components: {
-    ProductCard,
-    Cart,
-  },
-  async fetch() {
-    this.shop = await this.$axios.$get(`/shops/${this.$route.params.id}`)
-  },
-  data() {
-    return {
-      shop: {},
-    }
-  },
-  methods: {
-    seeProduct: function (productId) {
-      this.$router.push(`/products/${productId}`)
+  export default {
+    head() {
+      return {
+        title: `${this.shop.name} | FrenchCrafters`,
+        meta: [
+          {hid: 'checkout', name: 'description', content: `FrenchCrafters shop page for ${this.shop.name}`}
+        ]
+      }
     },
-  },
-}
+    components: {
+      ProductCard,
+      Cart,
+    },
+    async fetch() {
+      this.shop = await this.$axios.$get(`/shops/${this.$route.params.id}`)
+    },
+    data() {
+      return {
+        shop: {},
+      }
+    },
+    methods: {
+      seeProduct: function (productId) {
+        this.$router.push(`/products/${productId}`)
+      },
+    },
+  }
 </script>
