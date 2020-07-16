77b17c7298c5a82987d28895aa17cd6370cf9e98
thisisjuke <liege.arthur60@gmail.com>
2020-07-16 14:06:18 +0200

    :bug: forgot one input on register

--- front/pages/users/userRegister.vue
+++ front/pages/users/userRegister.vue
@@ -12,6 +12,12 @@
               <span class="border-b w-1/5 lg:w-1/4"></span>
             </div>
             <div class="mt-4">
+              <label class="block text-gray-700 text-sm font-bold mb-2">Nom d'utilisateur</label>
+              <input v-model="username"
+                     class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
+                     type="email">
+            </div>
+            <div class="mt-4">
               <label class="block text-gray-700 text-sm font-bold mb-2">Adresse Email</label>
               <input v-model="email"
                      class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
@@ -101,7 +107,7 @@
             this.$router.push('/login')
           } catch (err) {
             this.loading = false
-            alert(err.message || "An error occurred.")
+            this.$message("Oops.. Quelque chose s'est mal passé.")
           }
         }
       },
